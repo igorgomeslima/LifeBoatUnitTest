f9759e5c9349dcbd8bed4a41f3290d4aea50432b
Igor <Igor@DESKTOP-KHFASGT>
2018-08-18 11:06:26 -0300

    First changes.

---
+++ LifeBoatUnitTest.Logic/LifeBoatUnitTest.Logic.Test/FileProcessTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LifeBoatUnitTest.Logic.Test
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+            //Arrange
+            var fileProcess = new FileProcess();
+            bool resultMethodFileExists;
+
+            //Act
+            resultMethodFileExists = fileProcess.FileExists(@"C:\Windows\notepad.exe");
+
+            //Assert
+            Assert.IsTrue(resultMethodFileExists);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            //Arrange
+            var fileProcess = new FileProcess();
+            bool resultMethodFileExists;
+
+            //Act
+            resultMethodFileExists = fileProcess.FileExists(@"C:\:(.exe");
+
+            //Assert
+            Assert.IsFalse(resultMethodFileExists);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            //Arrange
+            var fileProcess = new FileProcess();
+
+            //Act
+            fileProcess.FileExists(string.Empty);
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException_UsingTryCatch()
+        {
+            //Arrange
+            var fileProcess = new FileProcess();
+
+            try
+            {
+                //Act
+                fileProcess.FileExists(string.Empty);
+            }
+            catch (Exception)
+            {
+                //Success
+                return;
+            }
+
+            //Assert
+            Assert.Fail("FileExists method did NOT throw an ArgumentNullException.");
+        }
+    }
+}
---
+++ LifeBoatUnitTest.Logic/LifeBoatUnitTest.Logic.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LifeBoatUnitTest.Logic.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LifeBoatUnitTest.Logic.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4588f373-388a-40fa-86e6-2e761a9cd094")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
